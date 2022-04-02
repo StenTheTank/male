56bc1d682e211e6634e822f62631667923538dcd
StenTheTank <sten11mnrgmail.com>
2022-04-02 20:02:34 +0300

    kuningas+legaalsuskontroll

--- Male/src/Kuningas.java
+++ Male/src/Kuningas.java
@@ -3,22 +3,98 @@ import java.util.Arrays;
 
 public class Kuningas extends Nupp{
     private boolean on_liigutatud;
+<<<<<<< HEAD
+    @Override
+    public void setAsukoht(int[] asukoht) {
+        super.setAsukoht(asukoht);
+        on_liigutatud = true;
+    }
+=======
 
+>>>>>>> 817b941d7348642b0c965ac6949acb69083ba159
     public Kuningas(char varv, int[] asukoht) {
         this.varv = varv;
         this.asukoht = asukoht;
     }
 
+<<<<<<< HEAD
+    public boolean abikuningas(int rida,int veerg, Malelaud malelaud){
+        if (rida<0 || veerg<0 ) return false;
+        if (rida>7 || veerg > 7) return false;
+        if (malelaud.getLaud()[rida][veerg]==null) return true;
+        else if (malelaud.getLaud()[rida][veerg].getVarv() != varv) return true;
+        return false;
+    }
+=======
     @Override
     public void setAsukoht(int[] asukoht) {
         super.setAsukoht(asukoht);
         on_liigutatud = true;
     }
 
+>>>>>>> 817b941d7348642b0c965ac6949acb69083ba159
     @Override
     public ArrayList<int[]> kaigud(Malelaud malelaud) {
+        ArrayList<int[]> tulemus = vangerdus(malelaud);
+        int rida=asukoht[0];int veerg=asukoht[1];
+        if (abikuningas(rida+1,veerg-1,malelaud)) tulemus.add(new int[]{rida+1,veerg-1});
+        if (abikuningas(rida+1,veerg,malelaud)) tulemus.add(new int[]{rida+1,veerg});
+        if (abikuningas(rida+1,veerg+1,malelaud)) tulemus.add(new int[]{rida+1,veerg+1});
+        if (abikuningas(rida,veerg+1,malelaud)) tulemus.add(new int[]{rida,veerg+1});
+        if (abikuningas(rida,veerg-1,malelaud)) tulemus.add(new int[]{rida,veerg-1});
+        if (abikuningas(rida-1,veerg-1,malelaud)) tulemus.add(new int[]{rida-1,veerg-1});
+        if (abikuningas(rida-1,veerg,malelaud)) tulemus.add(new int[]{rida-1,veerg});
+        if (abikuningas(rida-1,veerg+1,malelaud)) tulemus.add(new int[]{rida-1,veerg+1});
+        if (vangerdus(malelaud).size()>0){
 
-        return null;
+        }
+
+        return tulemus;
+    }
+    public ArrayList<int[]> vangerdus(Malelaud laud){
+        Vanker v1=(Vanker) laud.getLaud()[asukoht[0]][7];
+        Vanker v2=(Vanker) laud.getLaud()[asukoht[0]][0];
+        boolean valge_kaik;
+        ArrayList<int[]>vastus=new ArrayList<>();
+        if (varv=='v') valge_kaik=true;
+        else valge_kaik=false;
+        ArrayList<int[]>v_kaigud=vastasekaigud(laud,valge_kaik);
+        if (!on_liigutatud && !v1.isOn_liigutatud()){
+            if (!(v_kaigud.contains(new int[]{asukoht[0],5}) || v_kaigud.contains(new int[]{asukoht[0],4}))){
+                vastus.add(new int[]{asukoht[0],6});
+            }
+        }
+        if (!on_liigutatud && !v2.isOn_liigutatud()){
+            if (!(v_kaigud.contains(new int[]{asukoht[0],3}) || v_kaigud.contains(new int[]{asukoht[0],4}))){
+                vastus.add(new int[]{asukoht[0],2});
+            }
+        }
+        return vastus;
+    }
+    public ArrayList<int[]>vastasekaigud(Malelaud laud,boolean valge_kaik) {
+        ArrayList<int[]> vastus = new ArrayList<>();
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                if (laud.getLaud()[i][j]!=null) {
+                    if (valge_kaik) {
+                        if (laud.getLaud()[i][j].getVarv() == 'v') {
+                            ArrayList<int[]> ajutine = laud.getLaud()[i][j].kaigud(laud);
+                            for (int[] kaik : ajutine) {
+                                vastus.add(kaik);
+                            }
+                        }
+                    } else {
+                        if (laud.getLaud()[i][j].getVarv() == 'm') {
+                            ArrayList<int[]> ajutine = laud.getLaud()[i][j].kaigud(laud);
+                            for (int[] kaik : ajutine) {
+                                vastus.add(kaik);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return vastus;
     }
 
     @Override
--- Male/src/Peaklass.java
+++ Male/src/Peaklass.java
@@ -1,22 +1,99 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Objects;
 import java.util.Scanner;
 
 public class Peaklass {
+<<<<<<< HEAD
+    public static ArrayList<int[]> legaalsus_filter(ArrayList<int[]>kaigud,int[]asukoht,Malelaud praegune_malelaud,boolean valge_kaik) throws CloneNotSupportedException {
+        ArrayList<int[]>legaalsed_kaigud=new ArrayList<>();
+        Malelaud voimalik=(Malelaud) praegune_malelaud.clone();
+        for (int[] kaik : kaigud) {
+            if (legaalne(valge_kaik,voimalik)){
+                legaalsed_kaigud.add(kaik);
+            }
+
+        }
+        return legaalsed_kaigud;
+    }
+    public static ArrayList<int[]>vastasekaigud(Malelaud laud,boolean valge_kaik){
+        ArrayList<int[]>vastus=new ArrayList<>();
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                if (laud.getLaud()[i][j]!=null) {
+                    if (valge_kaik) {
+                        if (laud.getLaud()[i][j].getVarv() == 'v') {
+                            ArrayList<int[]> ajutine = laud.getLaud()[i][j].kaigud(laud);
+                            for (int[] kaik : ajutine) {
+                                vastus.add(kaik);
+                            }
+                        }
+                    } else {
+                        if (laud.getLaud()[i][j].getVarv() == 'm') {
+                            ArrayList<int[]> ajutine = laud.getLaud()[i][j].kaigud(laud);
+                            for (int[] kaik : ajutine) {
+                                vastus.add(kaik);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return vastus;
+    }
+
+    public static boolean legaalne(boolean valge_kaik,Malelaud laud){
+        Nupp[][]nupud=laud.getLaud();
+        ArrayList<int[]>v_kaigud = vastasekaigud(laud,valge_kaik);
+        int[]asukoht;
+        for (Nupp[] nupps : nupud) {
+            for (Nupp nupp : nupps) {
+                if (nupp!=null){
+                    if (valge_kaik) {
+                        if (nupp.getClass() == Kuningas.class) {
+                            if (nupp.getVarv() == 'v') {
+                                asukoht = nupp.getAsukoht();
+                                if (v_kaigud.contains(asukoht)) {
+                                    return false;
+                                }
+                            }
+                        }
+                    }
+                    else{
+                        if (nupp.getClass() == Kuningas.class) {
+                            if (nupp.getVarv() == 'm') {
+                                asukoht = nupp.getAsukoht();
+                                if (v_kaigud.contains(asukoht)) {
+                                    return false;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return true;
+    }
+=======
     private static int käiguarv = 1;
 
     public static int getKäiguarv() {
         return käiguarv;
     }
 
+>>>>>>> 817b941d7348642b0c965ac6949acb69083ba159
     public static void main(String[] args) {
+        boolean valge_kaik=true;
         Malelaud laud = new Malelaud();
         laud.väljasta();
+<<<<<<< HEAD
+=======
         System.out.println(Arrays.deepToString(laud.getLaud()[0][0].kaigud(laud).toArray()));
         System.out.println(laud.getLaud()[1][0].getClass() == Ettur.class);
         /*while(){
 
             käiguarv++;
         }*/
+>>>>>>> 817b941d7348642b0c965ac6949acb69083ba159
     }
 }
